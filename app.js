63e6eeb6de178e9dc3e583bc4f661e95dbe06b5a
Tomas Perez <tomaspz@gmail.com>
2020-03-29 00:29:06 -0400

    added email validation with a regular expression in the inquirer questions and it works

--- app.js
+++ app.js
@@ -5,64 +5,75 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+
+// let rightEmail = "tomaspz@yahoo.com"
+// let wrongEmail = "7@moc"
+// console.log("Right email: " + emailAnswerValidator(rightEmail));
+// console.log("Wrong email: " + emailAnswerValidator(wrongEmail));
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const emailAnswerValidator = async (input) => {
-    if(input !== ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$){
-        console.log("Please, type a correct email address!")
-    }
-    return;
-}
 
 inquirer.prompt([
     {
-      type: "input",
-      name: "name",
-      message: "What is the team member's name?"
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the team member id?",
+        type: "input",
+        name: "name",
+        message: "What is the team member's name?"
     },
     {
         type: "input",
-        name: "email",
-        message: "What is the team member id?",
-      },
+        name: "id",
+        message: "What is the team member's id?",
+    },
     {
-      type: "input",
-      message: "What is the team member email?",
-      name: "email",
-      validation: emailAnswerValidator
-    }
-  ]).then(answers => { 
+        type: "input",
+        message: "What is the team member's email?",
+        name: "email",
+        validate: function(value){
+            var pass = value.match(
+                /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/i
+                );
+            if (pass) {
+                return true;
+            } 
+            return "Please enter a valid email address!";      
+        }
+    },
+    {
+        type: "list",
+        message: "What is the team member's role?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"]
 
-  }).catch(error => { 
-    console.log("Sorry there was an error. Try again!")
-  });
-​
+    }
+]).then(answers => {
+    console.log(answers);
+}).catch(error => {
+    if (error) {
+        throw error;
+    }
+    console.log("Sorry there was an error. Try again!");
+});
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
